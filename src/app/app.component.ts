a28cc0ade0ced0cb63057814ed5a5ce15c1121b6
Jan <janek.lewandoski@gmail.com>
2020-07-30 00:21:18 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmedValidator } from '../app/validators/confirmed-validator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'form-builder-demo';
+export class AppComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+    },
+    {
+      validator: ConfirmedValidator('password', 'confirmPassword')
+    });
+  }
+
+  get lf() {
+    return this.loginForm.controls;
+  }
+
+  get email() {
+    return this.lf.email;
+  }
+
+  get password() {
+    return this.lf.password;
+  }
+
+  get confirmPassword() {
+    return this.lf.confirmPassword;
+  }
+
+  submit() {
+    // send this.loginForm.value to API
+    console.log(this.loginForm.value);
+  }
+
 }
